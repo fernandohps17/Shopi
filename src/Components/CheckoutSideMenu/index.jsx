ff97c29908fe7ba24d0ef95b6138a749a6ff98bf
Fernando Perez <fernandohps17@gmail.com>
2024-10-05 00:20:29 -0400

    Se genero el componente para agregar producto al carrito, orderCard

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -2,9 +2,11 @@ import { XMarkIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 import React, { useContext } from 'react'
 import './CheckoutSideMenu.scss';
+import OrderCard from '../OrderCard';
 
 const CheckoutSideMenu = () => {
-    const { closeCheckoutSideMenu, isCheckoutSideMenuOpen } = useContext(ShoppingCartContext)
+    const { closeCheckoutSideMenu, isCheckoutSideMenuOpen, cartProducts } = useContext(ShoppingCartContext)
+    console.log(cartProducts)
 
     return (
         <aside className={`${isCheckoutSideMenuOpen ? 'checkout_side_menu' : ''}`}>
@@ -13,6 +15,21 @@ const CheckoutSideMenu = () => {
                     <h2>My Order</h2>
                 </header>
                 <div className='checkout_side_menu_close' onClick={() => closeCheckoutSideMenu()}><XMarkIcon /></div>
+            </div>
+
+            <div className='order_card_container'>
+                {
+                    cartProducts.map((product => {
+                        return (
+                            <OrderCard
+                                key={product.id}
+                                title={product.title}
+                                imageUrl={product.images}
+                                price={product.price}
+                            />
+                        )
+                    }))
+                }
             </div>
 
         </aside>
--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -2,24 +2,27 @@ import { XMarkIcon } from '@heroicons/react/24/solid'
 import './OrderCard.scss'
 import React from 'react'
 
-const OrderCard = () => {
-  return (
-    <div className='order_card'>
-      <div>
-        <figure>
-            <img src="" alt="" />
-        </figure>
-        <div>
-            <p></p>
-        </div>
+const OrderCard = (props) => {
 
-        <div className=''>
-            <p></p>
-            <XMarkIcon />
+    const { id, title, imageUrl, price } = props
+// clase 18
+    return (
+        <div className='order_card'>
+            <div className='order_card_content'>
+                <figure className='order_card_content_image'>
+                    <img src={imageUrl} alt={title} />
+                </figure>
+                <div className='order_card_content_title'>
+                    <p>{title}</p>
+                </div>
+
+                <div className='order_card_content_price'>
+                    <p>{price}$</p>
+                    <XMarkIcon className='order_card_content_icon' />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default OrderCard
